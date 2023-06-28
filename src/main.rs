abd22406714745dbcfd316f79dd9b1ddca53100f
Tyler Harpool <tylerharpool@gmail.com>
2023-06-28 11:05:24 -0500

    Feature: Removed actix_web boilerplate and add healthcheck

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
+use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
+
+
+async fn health_check(req: HttpRequest) -> impl Responder {
+    HttpResponse::Ok().finish()
 }
+
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     HttpServer::new(|| {
         App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
+            .route("/health_check", web::get().to(health_check))
     })
     .bind("127.0.0.1:8000")?
     .run()
