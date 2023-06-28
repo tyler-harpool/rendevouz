6d1a62c4f67edeada4b9b47f7856ea3975efc0b0
Tyler Harpool <tylerharpool@gmail.com>
2023-06-28 18:19:35 -0500

    Feature: integration tests

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,6 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
-
-
-async fn health_check(req: HttpRequest) -> impl Responder {
-    HttpResponse::Ok().finish()
-}
+use rendevouz::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    run()?.await
 }
