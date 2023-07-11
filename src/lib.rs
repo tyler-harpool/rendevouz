6b1940341a1c7c45d8bbf577c874e40d1f7642ef
tyler-harpool <tylerharpool@gmail.com>
2023-07-11 13:24:18 -0500

    Added passing 200 subscription test

--- src/lib.rs
+++ src/lib.rs
@@ -7,9 +7,18 @@ async fn health_check() -> HttpResponse {
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+    let server = HttpServer::new(|| {
+        App::new()
+    .route("/health_check", web::get().to(health_check))
+    .route("/subscriptions", web::post().to(subscribe))
+
+})
         .listen(listener)?
         .run();
 
     Ok(server)
+}
+
+async fn subscribe() -> HttpResponse {
+    HttpResponse::Ok().finish()
 }
